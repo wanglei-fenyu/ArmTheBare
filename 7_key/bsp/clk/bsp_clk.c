73f517a2c15f42f2eb4c3e79c3e2453c74387b01
wanglei-fenyu <67000783+wanglei-fenyu@users.noreply.github.com>
2021-01-06 09:52:04 +0800

    按键驱动实验

---
+++ 7_key/bsp/clk/bsp_clk.c
@@ -0,0 +1,13 @@
+#include "bsp_clk.h"
+//使能外设时钟
+void clk_enable(void)
+{
+    
+    CCM->CCGR0 = 0xffffffff;
+    CCM->CCGR1 = 0xffffffff;
+    CCM->CCGR2 = 0xffffffff;
+    CCM->CCGR3 = 0xffffffff;
+    CCM->CCGR4 = 0xffffffff;
+    CCM->CCGR5 = 0xffffffff;
+    CCM->CCGR6 = 0xffffffff;
+}
